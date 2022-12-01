c465827769d59045c5503151dd6cfb8db55295e3
liac301 <115490478+liacr301@users.noreply.github.com>
2022-12-01 20:39:36 +0000

    Delete aula2 directory

--- aula2/aula02e05.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash 
-echo "$n Olá" 
-echo "Arg 1: $4 2 " 
-echo "Arg 2: $6 3" 
-echo "$#" 
-echo "$@ 46 345"
--- aula2/px.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/bash
-(($# < 1)) && echo -e "Indique ficheiros\n" || chmod u+x $*
